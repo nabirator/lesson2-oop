c25503c2b5e59eff0492d9d9914bd870f9cd2752
Yurii Zhuravlov <nabirator@gmail.com>
2018-10-31 07:57:06 +0200

    Added times and penalties

--- app/finish.php
+++ app/finish.php
@@ -5,23 +5,26 @@ use Gymkhana\Table;
 require __DIR__ . '/../data/database.php';
 require_once __DIR__ . '/../vendor/autoload.php';
 
+$table = new Table();
+$sorted_db = $database;
+foreach ($sorted_db as $key => $value) {
+  $time = random_int( 0, time() );
+  $sorted_db[$key]['time'] = date('H:i', $time);
+  $penalty_keys = ['рука', 'нога', 'лицо', 'конус'];
+  $penalty = array_rand($penalty_keys, 1);
+  $sorted_db[$key]['penalty'] = random_int(1, 10) . ' ' . $penalty_keys[$penalty];
+}
 if (PHP_SAPI === 'cli') {
   // In cli-mode
-  $table = new Table();
   //  echo $x->table($database);
-  foreach ($database as $key => $value) {
-    $time = random_int( 0, time() );
-    $database[$key]['time'] = date('H:i', $time);
-  }
-  echo $table->showTable($database);
+  echo $table->showTable($sorted_db);
 }
 else {
   // Not in cli-mode // From WEB.
   echo '<html><body>';
-  $table = new Table();
   echo '<h2>Table with time and penalty</h2>';
   try {
-    echo $table->showTimePenaltyTable($database);
+    echo $table->showTimePenaltyTable($sorted_db);
   } catch (Exception $e) {
   }
   echo '</html></body>';
--- app/start.php
+++ app/start.php
@@ -5,19 +5,18 @@ use Gymkhana\Table;
 require __DIR__. '/../data/database.php';
 require_once __DIR__.'/../vendor/autoload.php';
 
+$table = new Table();
+$sorted_db = $database;
+usort($sorted_db, function($a, $b) {
+  return $a['class'] <=> $b['class'];
+});
 if (PHP_SAPI === 'cli') {
   // In cli-mode
-  $table = new Table();
-  $sorted_db = $database;
-  usort($sorted_db, function($a, $b) {
-    return $a['class'] <=> $b['class'];
-  });
   echo $table->showTable($sorted_db);
 }
 else {
   // Not in cli-mode // From WEB.
   echo '<html><body>';
-  $table = new Table();
-  echo $table->showGroupedByClassTable($database);
+  echo $table->showGroupedByClassTable($sorted_db);
   echo '</html></body>';
 }
