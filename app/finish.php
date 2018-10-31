deded23f6ad40a0d7c588dcc71270cca7bc22df1
Yurii Zhuravlov <nabirator@gmail.com>
2018-10-31 08:19:14 +0200

    Added random round(s) from 1 to 5

--- app/finish.php
+++ app/finish.php
@@ -7,12 +7,15 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 $table = new Table();
 $sorted_db = $database;
-foreach ($sorted_db as $key => $value) {
-  $time = random_int( 0, time() );
-  $sorted_db[$key]['time'] = date('H:i', $time);
-  $penalty_keys = ['рука', 'нога', 'лицо', 'конус'];
-  $penalty = array_rand($penalty_keys, 1);
-  $sorted_db[$key]['penalty'] = random_int(1, 10) . ' ' . $penalty_keys[$penalty];
+$rounds = random_int(1, 5);
+for ($x = 0; $x < $rounds; $x++) {
+  foreach ($sorted_db as $key => $value) {
+    $time = random_int( 0, time() );
+    $sorted_db[$key]['time' . $x] = date('H:i', $time);
+    $penalty_keys = ['рука', 'нога', 'лицо', 'конус'];
+    $penalty = array_rand($penalty_keys, 1);
+    $sorted_db[$key]['penalty' . $x] = random_int(1, 10) . ' ' . $penalty_keys[$penalty];
+  }
 }
 if (PHP_SAPI === 'cli') {
   // In cli-mode
