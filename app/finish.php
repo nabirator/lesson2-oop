ca9f1a280d6312d8f8cde2b6aa9cba833cc796a5
Yurii Zhuravlov <nabirator@gmail.com>
2018-10-31 07:24:32 +0200

    Moving methods to class

--- app/finish.php
+++ app/finish.php
@@ -1,41 +1,28 @@
 <?php
 
-namespace App;
-require '../data/database.php';
+use Gymkhana\Table;
 
-class finish {
-  public function showTimePenaltyTable($database): string {
-    foreach ($database as &$row) {
-      srand(mktime(0, 0, 0));
-      $time = rand( 0, time() );
-      $row['time'] = date("H:i", $time);
-    }
+require __DIR__ . '/../data/database.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
-    $markup = '<table><tr><th>#</th><th>Класс</th><th>Участник</th><th>Транспорт</th><th>Время</th><th>Штраф</th></tr>';
-    foreach ($database as $row) {
-      $markup .= '<tr>';
-      // ID
-      $markup .= '<td>' . $row['id'] . '</td>';
-      // CLASS
-      $markup .= '<td>' . $row['class'] . '</td>';
-      // USER
-      $markup .= '<td>' . $row['name'] . '</td>';
-      // TRANSPORT
-      $markup .= '<td>' . $row['vehicle'] . '</td>';
-      // TIME
-      $markup .= '<td>' . $row['time'] . '</td>';
-      // PENALTY
-      $markup .= '<td>' . $row['penalty'] . '</td>';
-      $markup .= '</tr>';
-    }
-
-    $markup .= '</table></body></html>';
-    return $markup;
+if (PHP_SAPI === 'cli') {
+  // In cli-mode
+  $table = new Table();
+  //  echo $x->table($database);
+  foreach ($database as $key => $value) {
+    $time = random_int( 0, time() );
+    $database[$key]['time'] = date('H:i', $time);
   }
+  echo $table->showTable($database);
 }
-
-echo '<html><body>';
-$x = new finish();
-echo '<h2>Table with time and penalty</h2>';
-echo $x->showTimePenaltyTable($database);
-echo '</html></body>';
+else {
+  // Not in cli-mode // From WEB.
+  echo '<html><body>';
+  $table = new Table();
+  echo '<h2>Table with time and penalty</h2>';
+  try {
+    echo $table->showTimePenaltyTable($database);
+  } catch (Exception $e) {
+  }
+  echo '</html></body>';
+}
--- app/start.php
+++ app/start.php
@@ -1,119 +1,23 @@
 <?php
 
-namespace App;
-require '../data/database.php';
+use Gymkhana\Table;
 
-class start {
-  public function table($data) {
-
-    // Find longest string in each column
-    $columns = [];
-    foreach ($data as $row_key => $row) {
-      foreach ($row as $cell_key => $cell) {
-        $length = strlen($cell);
-        if (empty($columns[$cell_key]) || $columns[$cell_key] < $length) {
-          $columns[$cell_key] = $length;
-        }
-      }
-    }
-
-    // Output table, padding columns
-    $table = '';
-    foreach ($data as $row_key => $row) {
-      foreach ($row as $cell_key => $cell)
-        $table .= str_pad($cell, $columns[$cell_key]) . '   ';
-      $table .= PHP_EOL;
-    }
-    return $table;
-
-  }
-
-  public function tableSorted($data) {
-    $sorted_db = $data;
-    usort($sorted_db, function($a, $b) {
-      return $a['class'] <=> $b['class'];
-    });
-
-    // Find longest string in each column
-    $columns = [];
-    foreach ($sorted_db as $row_key => $row) {
-      foreach ($row as $cell_key => $cell) {
-        $length = strlen($cell);
-        if (empty($columns[$cell_key]) || $columns[$cell_key] < $length) {
-          $columns[$cell_key] = $length;
-        }
-      }
-    }
-
-    // Output table, padding columns
-    $table = '';
-    foreach ($sorted_db as $row_key => $row) {
-      foreach ($row as $cell_key => $cell)
-        $table .= str_pad($cell, $columns[$cell_key]) . '   ';
-      $table .= PHP_EOL;
-    }
-    return $table;
-
-  }
-
-  public function showTable($database) {
-    $markup = '<table><tr><th>#</th><th>Класс</th><th>Участник</th><th>Транспорт</th></tr>';
-
-    foreach ($database as $row) {
-      $markup .= '<tr>';
-      // ID
-      $markup .= '<td>' . $row['id'] . '</td>';
-      // CLASS
-      $markup .= '<td>' . $row['class'] . '</td>';
-      // USER
-      $markup .= '<td>' . $row['name'] . '</td>';
-      // TRANSPORT
-      $markup .= '<td>' . $row['vehicle'] . '</td>';
-      $markup .= '</tr>';
-    }
-
-    $markup .= '</table></body></html>';
-    return $markup;
-  }
-
-  public function showGroupedByClassTable($database) {
-    $sorted_db = $database;
-    usort($sorted_db, function($a, $b) {
-      return $a['class'] <=> $b['class'];
-    });
-    $markup = '<table><tr><th>#</th><th>Класс</th><th>Участник</th><th>Транспорт</th></tr>';
-
-    foreach ($sorted_db as $row) {
-      $markup .= '<tr>';
-      // ID
-      $markup .= '<td>' . $row['id'] . '</td>';
-      // CLASS
-      $markup .= '<td>' . $row['class'] . '</td>';
-      // USER
-      $markup .= '<td>' . $row['name'] . '</td>';
-      // TRANSPORT
-      $markup .= '<td>' . $row['vehicle'] . '</td>';
-      $markup .= '</tr>';
-    }
-
-    $markup .= '</table></body></html>';
-    return $markup;
-  }
-}
+require __DIR__. '/../data/database.php';
+require_once __DIR__.'/../vendor/autoload.php';
 
 if (PHP_SAPI === 'cli') {
   // In cli-mode
-  $x = new start();
-  echo $x->table($database);
-  echo $x->tableSorted($database);
+  $table = new Table();
+  $sorted_db = $database;
+  usort($sorted_db, function($a, $b) {
+    return $a['class'] <=> $b['class'];
+  });
+  echo $table->showTable($sorted_db);
 }
 else {
   // Not in cli-mode // From WEB.
   echo '<html><body>';
-  $x = new start();
-  echo '<h2>Initial table</h2>';
-  echo $x->showTable($database);
-  echo '<h2>Sorted by class table</h2>';
-  echo $x->showGroupedByClassTable($database);
+  $table = new Table();
+  echo $table->showGroupedByClassTable($database);
   echo '</html></body>';
 }
