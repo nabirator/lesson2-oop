7457eb04c9d1e30858958340f3d2661768fd1fab
Yurii Zhuravlov <nabirator@gmail.com>
2018-12-14 06:44:59 +0200

    Adding random penalties and calculating results.

--- src/Gymkhana/Service/Table.php
+++ src/Gymkhana/Service/Table.php
@@ -9,7 +9,6 @@ class Table
     public function showTable($data): string
     {
         $markup = '<table><tr><th>#</th><th>Класс</th><th>Участник</th><th>Транспорт</th></tr>';
-
         foreach ($data as $row) {
             $markup .= '<tr>';
             // ID
@@ -29,13 +28,12 @@ class Table
 
     public function showGroupedByClassTable($data): string
     {
-        $sorted_db = $data;
-        usort($sorted_db, function ($a, $b) {
+        usort($data, function ($a, $b) {
             return $a['class'] <=> $b['class'];
         });
         $markup = '<table><tr><th>#</th><th>Класс</th><th>Участник</th><th>Транспорт</th></tr>';
 
-        foreach ($sorted_db as $row) {
+        foreach ($data as $row) {
             $markup .= '<tr>';
             // ID
             $markup .= '<td>' . $row['id'] . '</td>';
@@ -54,33 +52,59 @@ class Table
 
     public function showTimePenaltyTable($data): string
     {
-        $data['rounds'] = random_int(1, 5);
+        date_default_timezone_set('UTC');
+        $rounds = random_int(1, 5);
         foreach ($data as $key => $value) {
-            $time = random_int(0, time());
-            if (\is_int($key)) {
-                $data[$key]['time'] = date('H:i', $time);
-                for ($x = 0; $x < $data['rounds']; $x++) {
-                    $time = random_int(0, time());
-                    $data[$key]['time' . $x] = date('H:i:s', $time);
-                    $penalty_keys = ['рука', 'нога', 'лицо', 'конус'];
-                    $penalty = array_rand($penalty_keys, 1);
-                    $penalty_points = random_int(1, 10);
-                    $penalty_time = $time + $penalty_points;
-                    $result = date('H:i:s', $penalty_time);
-                    $data[$key]['penalty' . $x] = $penalty_points . ' ' . $penalty_keys[$penalty];
-                    if (!isset($data[$key]['result'])) {
-                        $data[$key]['result'] = $result;
-                    }
+            for ($x = 1; $x <= $rounds; $x++) {
+                $random_result = random_int(0, 99);
+                switch (true) {
+                    case ($random_result < 5):
+                        // 5% possibility to empty result.
+                        $data[$key]['time'][$x] = '';
+                        $data[$key]['penalty'][$x] = '';
+                        if (!isset($data[$key]['result'])) {
+                            $data[$key]['result'] = '';
+                        }
+                        break;
+                    case ($random_result >= 90):
+                        // 10% possibility to not count result.
+                        $data[$key]['time'][$x] = 'Не засчитано';
+                        $data[$key]['penalty'][$x] = '';
+                        if (!isset($data[$key]['result'])) {
+                            $data[$key]['result'] = 'Не засчитано';
+                        }
+                        break;
+                    default:
+                        // Random time from 40 minutes to 1 hour 30 minutes.
+                        $data[$key]['time'][$x] = random_int(2400, 5400);
+                        $penalty_keys = ['рука', 'нога', 'лицо', 'конус'];
+                        $penalty = array_rand($penalty_keys, 1);
+                        $is_penalty = random_int(0, 1);
+                        $penalty_points = random_int(1, 4);
+                        if ($is_penalty) {
+                            $result = $data[$key]['time'][$x] + $penalty_points;
+                            $data[$key]['penalty'][$x] = $penalty_points . ' ' . $penalty_keys[$penalty];
+                        } else {
+                            $result = $data[$key]['time'][$x];
+                            $data[$key]['penalty'][$x] = '';
+                        }
+                        if (!isset($data[$key]['result'])
+                            || (\is_int($result && !\is_int($data[$key]['result']))
+                            || (\is_int($result) && $data[$key]['result'] > $result))) {
+                            $data[$key]['result'] = $result;
+                        }
                 }
             }
         }
 
-        $markup = '<table><tr><th>#</th><th>Класс</th><th>Участник</th><th>Транспорт</th>';
-        for ($x = 0; $x < $data['rounds']; $x++) {
-            $markup .= "<th>Время $x</th><th>Штраф $x</th>";
+        $markup = '<table><tr><th>#</th><th>Класс</th><th>Участник</th>';
+        $rounds = \count($data[0]['time']);
+        for ($x = 1; $x <= $rounds; $x++) {
+            $markup .= '<th colspan="2">Заезд ' . $x . '</th>';
         }
-
+        $markup .= '<th>Лучший заезд</th><th>Транспорт</th>';
         $markup .= '</tr>';
+
         foreach ($data as $row) {
             $markup .= '<tr>';
             // ID
@@ -89,14 +113,17 @@ class Table
             $markup .= '<td>' . $row['class'] . '</td>';
             // USER
             $markup .= '<td>' . $row['name'] . '</td>';
-            // TRANSPORT
-            $markup .= '<td>' . $row['vehicle'] . '</td>';
-            for ($x = 0; $x < $data['rounds']; $x++) {
+            for ($x = 1; $x <= $rounds; $x++) {
                 // TIME
-                $markup .= '<td>' . $row["time$x"] . '</td>';
+                $markup .= '<td>' . (\is_int($row['time'][$x]) ?
+                    date('H:i:s', $row['time'][$x]) :
+                    $row['time'][$x]) . '</td>';
                 // PENALTY
-                $markup .= '<td>' . $row["penalty$x"] . '</td>';
+                $markup .= '<td>' . $row['penalty'][$x] . '</td>';
             }
+            $markup .= '<td>' . (\is_int($row['result']) ?
+                date('H:i:s', $row['result']) :
+                $row['result']) . '</td><td>' . $row['vehicle'] . '</td>';
             $markup .= '</tr>';
         }
 
